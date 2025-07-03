20beb7e743f07cfdb2bd526c8532281d9b3506e7
neelmyna <nithincheeta@gmail.com>
2025-07-03 11:42:27 +0530

    create_db function defined

--- week2_dsa/employee_crud_mysql.py
+++ week2_dsa/employee_crud_mysql.py
@@ -1,12 +1,33 @@
 import pymysql
 
-def connectDb():
+def connect_db():
     connection = None
     try:
-        connection = pymysql.Connect(host="localhost", user="root", password="root123", datebase="nithin_db", port=3306, charset="utf-8")
+        connection = pymysql.Connect(host='localhost', user="root", password="root123", datebase='nithin_db', port=3306, charset="utf8")
         print('Database Connected')
     except:
         print('Database Connection Failed')
     return connection
 
-connection = connectDb()
+def disconnect_db(connection):
+    try:
+        connection.close()
+        print('DB disconnected')
+    except:
+        print('DB disconnection failed')
+
+def creat_db():
+    query = 'create database IF NOT EXISTS nithin_db'
+    connection = connect_db()
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        print('Database created')
+        cursor.close()
+        disconnect_db(connection)
+    except:
+        print('Database creation failed')
+
+connection = connect_db()
+# connection.close() # to disconnect the DB
+disconnect_db(connection)
