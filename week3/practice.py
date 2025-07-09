49071f333758a74d02c0bb64b1dccefc80441c41
neelmyna <nithincheeta@gmail.com>
2025-07-09 12:00:29 +0530

    session15 notes added

--- week3/practice.py
+++ week3/practice.py
@@ -1,10 +1,8 @@
 import numpy as np
 
-array1 = np.zeros(3)
-array2 = np.zeros((1, 4))
-array3 = np.zeros((2, 5))
+array = np.array([2, 4, 6, 8, 9, 19])
 
-print(array1[0], array1[2])
-print(array2[0][0], array2[0][3])
-print(array3[1][0], array3[1][3])
-print(array1[0][0]) # Index Error
+array2 = array + 5 # Broadcasting. Adding a scalar quantity to every element of the array
+
+print(array)
+print(array2)
