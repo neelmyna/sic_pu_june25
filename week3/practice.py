fb68d980d82dd87758369f509ffe3d4925b135da
neelmyna <nithincheeta@gmail.com>
2025-07-10 14:49:18 +0530

    requests and flask programs added

--- week3/practice.py
+++ week3/practice.py
@@ -1,8 +1,14 @@
-import numpy as np
+import re
 
-array = np.array([2, 4, 6, 8, 9, 19])
+words = ["apple", "banana", "orange", "grape", "umbrella"]
 
-array2 = array + 5 # Broadcasting. Adding a scalar quantity to every element of the array
+# Capitalize if starts with a vowel
+vowel_caps = list(map(lambda w: re.sub(r'^[aeiou]', lambda m: m.group(0).upper(), w), words))
+print(vowel_caps)
 
-print(array)
-print(array2)
+# Capitalize the whole word if it starts with a vowel
+vowel_caps = list(map(lambda w: re.sub(r'^[aeiou](\w+)', w.upper(), w), words))
+print(vowel_caps)
+
+# re.sub(r'[^a-zA-Z]
+# lambda s: re.sub(r'[^a-zA-Z]', '', s
