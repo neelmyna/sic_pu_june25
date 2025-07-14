c9177baecb06d5290af7739ef12479cfed65890c
neelmyna <nithincheeta@gmail.com>
2025-07-14 10:54:48 +0530

    pandas

--- week3/practice.py
+++ week3/practice.py
@@ -1,3 +1,8 @@
+import numpy as np
+# Replacing values less than 50 with 0 in an array
 
+# Explanation: Using np.where() to replace values meeting the condition
 
-print('\'Nithin\' is the best trainer available in Karnataka\'s city \"Mysuru\"')
+expenses = np.array([20, 60, 5, 80, 45, 90])
+modified_expenses = np.where(expenses < 50, 0, expenses)
+print(modified_expenses)  # Output: [ 0 60  0 80  0 90]
