75cfe6e69eb0f062bafb509b87b66cae19d314b2
neelmyna <nithincheeta@gmail.com>
2025-07-15 15:37:58 +0530

    CHAPTER6 ASSESSMENT LINK ADDED

--- week3/practice.py
+++ week3/practice.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D  # Importing 3D plotting module
 
-sizes = [30, 20, 25, 25]
-labels = ['A', 'B', 'C', 'D']
-colors = ['blue', 'red', 'green', 'purple']
+fruits = ['Mango', 'Apple', 'Banana', 'Grapes']
+sales = [10, 20, 15, 25]
 
-# Create pie chart
-plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
-plt.title("Pie Chart")
+plt.bar(fruits, sales, color=['blue', 'green', 'red', 'purple'])
+plt.xlabel("Fruits")
+plt.ylabel("Sales (lakh rupees)")
+plt.legend()
+plt.title("Bar Chart")
 plt.show()
